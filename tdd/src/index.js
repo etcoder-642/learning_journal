6f651d253defc827b76814ca1dc3ce93b1c4960c
mnasie <manassehsamuel642@gmail.com>
2026-03-28 18:16:39 +0300

    Added all necessary tests required

--- tdd/src/index.js
+++ tdd/src/index.js
@@ -2,3 +2,63 @@
 export function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+export function calculator() {
+    return {
+        add: (a, b) => { return a + b; },
+        subtract: (a, b) => { return a - b; },
+        multiply: (a, b) => { return a * b; },
+        divide: (a, b) => { return a / b; }
+    };
+}
+
+export function ceasarCipher(str, shift) {
+    let char;
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == " ") { res += str[i]; continue; }
+        char = str.charCodeAt(i);
+        char += shift % 26;
+        res += ((str.charCodeAt(i) <= 90 && char >= 90) ||
+            (str.charCodeAt(i) <= 122 && char >= 122)) ?
+            String.fromCharCode(char - 26) : String.fromCharCode(char);
+    }
+    return res;
+}
+
+export function analyzeArray(arr) {
+    if(arr.length === 0) return {
+        average: null,
+        min: null,
+        max: null,
+        length: 0
+    }
+    function average(sample) {
+        return sample.reduce((pre, cur) => { return pre + cur }, 0) / arr.length;
+    }
+    function min(sample) {
+        let min = Infinity;
+        for (let i = 0; i < sample.length; i++) {
+            if (sample[i] < min) min = sample[i];
+        }
+        return min;
+    }
+    function max(sample) {
+        let max = -Infinity;
+        for (let i = 0; i < sample.length; i++) {
+            if (sample[i] > max) max = sample[i];
+        }
+        return max;
+
+    }
+    return {
+        average: average(arr),
+        min: min(arr),
+        max: max(arr),
+        length: arr.length
+    }
+}
--- tdd/src/index.test.js
+++ tdd/src/index.test.js
@@ -1,8 +1,56 @@
 import { capitalize } from "./index.js";
+import { reverseString } from "./index.js";
+import { calculator } from "./index.js";
+import { ceasarCipher } from "./index.js";
+import { analyzeArray } from "./index.js";
 
 test("Capitalize the first letter of a string", () => {
     expect(capitalize("hello")).toBe("Hello");
     expect(capitalize("TEST")).toBe("TEST");
     expect(capitalize("")).toBe("");
     expect(capitalize("a")).toBe("A");
+})
+
+test("reversing string", () => {
+    expect(reverseString("hello")).toBe("olleh");
+    expect(reverseString("TEST")).toBe("TSET");
+    expect(reverseString("")).toBe("");
+    expect(reverseString("a")).toBe("a");
+})
+
+test("calculator", () => {
+    expect(calculator().add(2, 3)).toBe(5);
+    expect(calculator().subtract(5, 2)).toBe(3);
+    expect(calculator().multiply(2, 3)).toBe(6);
+    expect(calculator().divide(6, 2)).toBe(3);
+})
+
+test("ceasarCipher", () => {
+    expect(ceasarCipher("hello", 1)).toBe("ifmmp");
+    expect(ceasarCipher("xyz", 3)).toBe("abc");
+    expect(ceasarCipher("hElLo", 5)).toBe("mJqQt");
+    expect(ceasarCipher("abc", 0)).toBe("abc");
+    expect(ceasarCipher("abc", 26)).toBe("abc");
+    expect(ceasarCipher("", 5)).toBe("");
+})
+
+test("analyze Array", () => {
+    expect(analyzeArray([1, 2, 3, 4, 5])).toEqual({
+        average: 3,
+        min: 1,
+        max: 5,
+        length: 5
+    });
+    expect(analyzeArray([5, 5, 5, 5])).toEqual({
+        average: 5,
+        min: 5,
+        max: 5,
+        length: 4
+    });
+    expect(analyzeArray([])).toEqual({
+        average: null,
+        min: null,
+        max: null,
+        length: 0
+    });
 })
