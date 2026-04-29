75721d7ee0dab0552c53e59a4bc0a8eeaa6a3467
mnasie <manassehsamuel642@gmail.com>
2026-04-29 15:52:01 +0300

    solved the pascal triangle problem

--- recursion/5_pascal/pascal.js
+++ recursion/5_pascal/pascal.js
@@ -1,6 +1,15 @@
-const pascal = function() {
-  
+const pascal = function(num) {
+  if(num == 1) return [1];
+  let universe = pascal(num-1);
+  let res = [1];
+  for(let i = 0; i < universe.length-1;i++){
+    res.push(universe[i]+universe[i+1]);
+  }
+  res.push(1);
+  return res;
 };
+
+console.dir(pascal(8), { depth: null });
   
 // Do not edit below this line
 module.exports = pascal;
